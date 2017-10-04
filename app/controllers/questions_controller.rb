3665cbba6d8d84b6aa349e7a6fb080fc3131701c
andrey <ag82pishi@yandex.ru>
2017-10-04 21:01:51 +0300

    questions and rspec(continue)

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_action :set_question, only: [:show, :edit]
+  before_action :set_question, only: [:show, :edit, :update, :destroy]
 
   def index
     @questions = Question.all
@@ -12,12 +12,41 @@ class QuestionsController < ApplicationController
     @question = Question.new
   end
 
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to @question
+    else
+      render :new
+    end
+  end
+
   def edit
+  end
+
+  def update
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @question.destroy
+      redirect_to questions_path
+    else
+      redirect_to :back
+    end
   end
 
   private
 
   def set_question
     @question = Question.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:title, :body)
   end
 end
