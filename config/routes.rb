be0f6676f23d800dbe25e0c22cd59bd70521d25a
andrey <ag82pishi@yandex.ru>
2018-03-26 17:09:41 +0300

    improving comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  # concern позволяет выносить общие части в роутах и переиспользовать их
   concern :commentable do
     resources :comments
   end
 
+  #concerns - подключил ресурс comments как вложенный
   resources :questions, concerns: :commentable do
     resources :answers
   end
