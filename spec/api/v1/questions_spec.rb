de460764585158849b0af8a87e067e8d3245d5e2
andrey <ag82pishi@yandex.ru>
2018-05-21 01:27:47 +0300

    API: added questions#index

--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -18,10 +18,28 @@ describe 'Questions API' do
       let(:user) { create(:user) }
       let!(:application) { Doorkeeper::Application.create!(name: 'Test', redirect_uri: 'urn:ietf:wg:oauth:2.0:oob', uid: '123456789', secret: '987654321') }
       let!(:access_token) { Doorkeeper::AccessToken.create!(application_id: application.id, resource_owner_id: user.id, scopes: 'public') }
+      let!(:questions) { create_list(:question, 2) }
+
+      before { get '/api/v1/questions', params: { format: :json, access_token: access_token.token,  } }
 
       it 'returns 200 status code' do
-        get '/api/v1/questions', params: { format: :json, access_token: access_token.token }
         expect(response).to be_success
+      end
+
+      it 'valid factory' do
+        puts "QUESTIONS: #{questions}"
+        expect(questions.count).to eq 2
+      end
+
+      it 'return list of questions' do
+        expect(response.body).to have_json_size(2)
+      end
+
+      %w(id title body created_at updated_at).each do |attr|
+        it "question object contains #{attr}" do
+          question = questions.first
+          expect(response.body).to be_json_eql(question.send(attr.to_sym).to_json).at_path("0/#{attr}")
+        end
       end
     end
   end
